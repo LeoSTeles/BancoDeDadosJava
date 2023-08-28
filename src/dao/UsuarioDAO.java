f0c6a1a241ac77312c9ddcca3ceb61d82ec692cc
Leonardo Teles <leo_steles@hotmail.com>
2023-08-28 13:34:04 -0300

    Criando métodos de manipulação do Banco de Dados na classe DAO

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -8,8 +8,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import model.Usuario;
 import view.FormCadastroView;
 
@@ -25,12 +27,75 @@ public class UsuarioDAO {
     }
     
     public void insert(Usuario usuario) throws SQLException{
-            String sql = "INSERT INTO usuarios(usuario,senha) VALUES (?,?)";
+        String sql = "INSERT INTO usuarios(usuario,senha) VALUES (?,?)";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        //Evitando Sql Injection
+        statement.setString(1, usuario.getUsuario());
+        statement.setString(2, usuario.getSenha());
+        statement.execute();
+    }
+    
+    public void update(Usuario usuario) throws SQLException{
+        String sql = "UPDATE INTO usuarios set usuario = ? and senha = ? where id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        //Evitando Sql Injection
+        statement.setString(1, usuario.getUsuario());
+        statement.setString(2, usuario.getSenha());
+        statement.setInt(3, usuario.getId());
+        statement.execute();
+    }
+    
+    public void insertOrUpdate(Usuario usuario) throws SQLException{
+        if(usuario.getId() > 0){
+            this.update(usuario);
+        }else{
+            this.insert(usuario);
+        }
+    }
+    
+    public void delete(Usuario usuario)throws SQLException{
+        if(usuario.getId() > 0){
+            String sql = "DELETE FROM usuarios where id = ?";
             PreparedStatement statement = connection.prepareStatement(sql);
             //Evitando Sql Injection
-            statement.setString(1, usuario.getUsuario());
-            statement.setString(2, usuario.getSenha());
+            statement.setInt(1, usuario.getId());
             statement.execute();
+        }else{
+            JOptionPane.showMessageDialog(null, "O usuário não existe, portanto não pode ser deletado.");
+        }
+    }
+    
+    public ArrayList<Usuario> selectAll() throws SQLException{
+        String sql = "SELECT * FROM usuarios";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        return pesquisa(statement);
+    }
+
+    private ArrayList<Usuario> pesquisa(PreparedStatement statement) throws SQLException {
+        //Criando array list de usuarios
+        ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+        statement.execute();
+        //Recebendo uma lista com todos os usuarios encontrados no banco
+        ResultSet resultSet = statement.getResultSet();
+        while(resultSet.next()){
+            int id = resultSet.getInt("id");
+            String usuario = resultSet.getString("usuario");
+            String senha = resultSet.getString("senha");
+            //Criando variável tipo usuário para armazenar dados da linha atual retornada do banco
+            Usuario user = new Usuario(id, usuario, senha);
+            //Armazenando o usuário da linha atual no array list criado
+            usuarios.add(user);
+        }
+        return usuarios;
+    }
+    
+    public Usuario selectById(Usuario usuario) throws SQLException{
+        String sql = "SELECT * FROM usuarios where id = ?";
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, usuario.getId());
+        statement.execute();
+
+        return pesquisa(statement).get(0);
     }
 
     public boolean verificarSeExiste(Usuario usuario) throws SQLException {
---
+++ src/view/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view;
+
+import dao.UsuarioDAO;
+
+/**
+ *
+ * @author leona
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
